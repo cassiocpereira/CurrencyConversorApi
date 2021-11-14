48b71a100b876c2ee4611380f0b31bead5b14445
Cassio <pereiraccassio@gmail.com>
2021-11-14 11:36:24 -0300

    Iniciando projeto

---
+++ src/main/java/com/conversor/currency/CurrencyConversorApiApplication.java
@@ -0,0 +1,13 @@
+package com.conversor.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conversor/currency/CurrencyConversorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversor.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
